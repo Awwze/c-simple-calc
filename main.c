4a8526321a4f9b8dc0c269ea734e02e18e0ba831
Awwze <thefloopa@gmail.com>
2024-12-15 20:25:00 +0500

    going to bed, this what i can to write

--- main.c
+++ main.c
@@ -1,3 +1,33 @@
-//
-// Created by MSI on 15.12.2024.
-//
+#include <stdio.h>
+
+int main(){
+    double num1;
+    double num2;
+    double result;
+    char act;
+
+    printf("Hello to simple calculator made in C by Awwze! \n");
+    printf("Choose your first number: ");
+    scanf("%lf", &num1);
+    printf("Choose your second number: ");
+    scanf("%lf", &num2);
+
+    printf("Nice, now choose your action (+ - * / ): \n");
+    scanf("%c", act);
+
+    switch(act){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result  = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+    }
+
+
+}
